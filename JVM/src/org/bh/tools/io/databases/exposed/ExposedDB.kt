45a0fd29d47ba38eb0bb50eea45de2b668484868
Ben Leggiero <BenLeggiero@Gmail.com>
2018-07-22 21:25:02 -0400

    Exposed...?

---
+++ JVM/src/org/bh/tools/io/databases/exposed/ExposedDB.kt
@@ -0,0 +1,15 @@
+package org.bh.tools.io.databases.exposed
+
+import org.bh.tools.io.databases.*
+
+
+/**
+ * @author Ben
+ * @since 2018-07-22
+ */
+class ExposedDB: Database<Any, Any, ExposedDB> {
+    override fun retrieve(using: Any): Any {
+
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
