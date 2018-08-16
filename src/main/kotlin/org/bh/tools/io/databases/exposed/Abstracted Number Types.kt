d88bb833c03e4a20721b2bf084775c9a22bee682
Ben Leggiero <BenLeggiero@Gmail.com>
2018-08-15 22:59:02 -0400

    Added abstract number types for Exposed

---
+++ src/main/kotlin/org/bh/tools/io/databases/exposed/Abstracted Number Types.kt
@@ -0,0 +1,12 @@
+package org.bh.tools.io.databases.exposed
+
+import org.jetbrains.exposed.dao.*
+
+/*
+ * @author Ben Leggiero
+ * @since 2018-08-05
+ */
+
+typealias IntegerIdTable = LongIdTable
+typealias IntegerEntity = LongEntity
+typealias IntegerEntityClass<Entity> = LongEntityClass<Entity>
